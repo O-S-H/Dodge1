f19df6d6c4802d1f7ffba2464248427eaeff5437
O-S-H <dhtjdgus153@naver.com>
2022-02-09 12:47:27 +0900

    2-9
    수업

--- Dodge/Assets/02.Scripts/GameManager.cs
+++ Dodge/Assets/02.Scripts/GameManager.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject gameObjectText;
+    public GameObject gameoverText;
     public Text timetext;// 생존시간을 표시할 텍스트 컴포넌트
     public Text recordText; // 최고 기록을 표시할 컴포넌트
 
@@ -23,9 +23,27 @@ public class GameManager : MonoBehaviour
   
     void Update()
     {
-        
-    }
+        if (!isGameover)
+        {
 
+
+            surviveTime += Time.deltaTime;
+            timetext.text = "Time : " + (int)surviveTime;  // timetext. text(오브젝트 내에 있는 필드(변수))  //debug.log 랑 똑같다. ""뒤에는 유니티가 뒷열을 string으로 형변환 해준다.
+        }
+
+        else
+        {
+            if(Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene("smapleScene");
+            }
+        }
+    }
+    public void Endgame()
+    {
+        isGameover = true;
+        gameoverText.SetActive(true);
+    }
     
     
     
